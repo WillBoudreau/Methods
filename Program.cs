12e33008392d43657c18561be34bebdff530f7ee
WillBoudreau <williamboudreau804@gmail.com>
2023-09-19 15:40:50 -0300

    Added a damage method and modified it

--- Program.cs
+++ Program.cs
@@ -8,15 +8,42 @@ namespace Methods
 {
     internal class Program
     {
-        static void Main(string[] args)
+        // int varaibles
+        int MonsterDamage = 100;
+        //Float varaibles
+        float shield = 100;
+        float health = 100;
+        static void Main()
         {
-
+            Console.WriteLine("Welcome Exploerer! You have been selected to go on a magical adventure through a land of wonders!");
+            Console.WriteLine("--------------------------------------------------------------------------------------------------");
+            Console.ReadKey();
         }
-        static void Damage()
+         void Damage()
         {
-
+            while(health >0)
+            {
+                if (shield > 0)
+                {
+                    Console.WriteLine("The monster attacks");
+                    shield = shield - MonsterDamage;
+                }
+                else
+                {
+                    Console.WriteLine("The monster attacks");
+                    health = health - MonsterDamage;    
+                }
+            } 
         }
         static void EXP()
+        {
+
+        }
+        static void EnemyBasic()
+        {
+
+        }
+        static void Mission1()
         {
 
         }
